95243bd9830786adb767c4e38979abaa23c840ef
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-28 20:07:35 -0300

    new: player controller class

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("CORE")]
+    protected InputStruct input;
+
+    [Header("CAMERA")]
+    private Transform cameraHandler = null;
+
+    #region MonoBehaviour
+
+    private void Start() {
+        //camera handler reference
+        cameraHandler = Camera.main.transform.parent;
+
+        //setup cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update() {
+        input = InputManager.CaptureInput();
+
+        LookAtCamera();
+    }
+
+    #endregion
+
+    #region GameplayMethods
+
+    private void LookAtCamera() {
+        transform.rotation = Quaternion.Euler(0f, input.look.x, 0f);
+        cameraHandler.transform.localRotation = Quaternion.Euler(-input.look.y, 0f, 0f);
+    }
+
+    #endregion
+}
--- Assets/Scripts/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -4,30 +4,8 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
-    [Header("CORE")]
-    protected InputStruct input;
-
-    [Header("CAMERA")]
-    private Transform cameraHandler = null;
-
-    protected override void Start() {
-        //camera handler reference
-        cameraHandler = Camera.main.transform.parent;
-
-        //setup cursor
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible   = false;
-    }
-
-    protected override void Update() {
-        base.Update();
-        
-        input = InputManager.CaptureInput();
-
-        LookAtCamera();
-    }
-
     protected override StatesTypes GetCurrentState() {
+        InputStruct input = InputManager.CaptureInput();
         if (input.move.magnitude != 0)
         {
             return StatesTypes.Walk;
@@ -35,13 +13,4 @@ public class PlayerStateMachine : StateMachine
 
         return StatesTypes.Idle;
     }
-
-    #region GameplayMethods
-
-    private void LookAtCamera() {
-        transform.rotation = Quaternion.Euler(0f, input.look.x, 0f);
-        cameraHandler.transform.localRotation = Quaternion.Euler(-input.look.y, 0f, 0f);
-    }
-
-    #endregion
 }
