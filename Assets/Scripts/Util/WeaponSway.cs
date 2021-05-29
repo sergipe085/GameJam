8bd6009a587881724743b3db0a45f771e03d3262
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-28 22:54:52 -0300

    weapon sway and weapon attack animation

--- Assets/Scripts/Util/WeaponSway.cs
+++ Assets/Scripts/Util/WeaponSway.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class WeaponSway : MonoBehaviour
 {
-    
+    private Vector3 initialPosition;
+
+    private void Start() {
+        initialPosition = transform.localPosition;
+    }
+
+    private void Update() {
+        float horizontal = -Input.GetAxis("Mouse X") * 0.5f;
+        float vertical   = -Input.GetAxis("Mouse Y") * 0.5f;
+
+        Vector3 targetPosition = new Vector3(horizontal, vertical, 0f);
+        transform.localPosition = Vector3.Lerp(transform.localPosition,  initialPosition + targetPosition, 5f * Time.deltaTime);    
+    }
 }
